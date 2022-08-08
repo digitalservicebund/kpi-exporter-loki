e621438d6c62a5598f807bd6df140e6b737f451d
Niko Felger <niko.felger@gmail.com>
2022-08-08 17:42:11 +0200

    Extract Config object

--- query_and_submit.py
+++ query_and_submit.py
@@ -1,34 +1,46 @@
 import datetime as dt
 import json
 import os
+from typing import Any, List
 import requests
 import subprocess
 import yaml
+from dataclasses import dataclass
 
 
-def load_config(filename):
+@dataclass
+class Config:
+    interval_start: str = ''
+    interval_end: str = ''
+    metrics_token: str = os.environ.get("METRICS_WEBHOOK_TOKEN")
+
+    def __post_init__(self):
+        one_hour_ago = dt.datetime.now() - dt.timedelta(hours=1)
+        self.interval_start = one_hour_ago.strftime("%Y-%m-%dT%H:00:00Z")
+        self.interval_end = one_hour_ago.strftime("%Y-%m-%dT%H:59:59Z")
+
+
+def load_endpoint(filename):
     with open(filename) as f:
         return yaml.safe_load(f)
 
 
-def process_endpoint_block(
-    endpoint_config, interval_start, interval_end, metrics_token
-):
-    metrics = query_metrics(endpoint_config["queries"], interval_start, interval_end)
-    post_metrics(endpoint_config["endpoint"], interval_start, interval_end, metrics)
+def process_endpoint(endpoint, config):
+    metrics = query_metrics(endpoint["queries"], config)
+    post_metrics(endpoint["endpoint"], config, metrics)
 
 
-def query_metrics(query_config, interval_start, interval_end):
+def query_metrics(queries, config):
     metrics = {}
 
-    for metric_name, query in query_config.items():
+    for metric_name, query in queries.items():
         output = json.loads(
             subprocess.check_output(
                 [
                     "/logcli-linux-amd64",
                     "instant-query",
                     "--now",
-                    interval_end,
+                    config.interval_end,
                     f"sum(count_over_time({query} [1h]))",
                 ]
             )
@@ -38,25 +50,22 @@ def query_metrics(query_config, interval_start, interval_end):
     return metrics
 
 
-def post_metrics(endpoint, interval_start, interval_end, metrics):
-    requests.post(
+def post_metrics(endpoint, config, metrics):
+    response = requests.post(
         url=endpoint,
         json={
-            "startInterval": interval_start,
-            "endInterval": interval_end,
+            "startInterval": config.interval_start,
+            "endInterval": config.interval_end,
             **metrics,
         },
         headers={
-            "xc-token": metrics_token,
+            "xc-token": config.metrics_token,
         },
     )
+    response.raise_for_status()
 
 
 if __name__ == "__main__":
-    config = load_config("/opt/config.yaml")
-    metrics_token = os.environ.get("METRICS_WEBHOOK_TOKEN")
-    one_hour_ago = dt.datetime.now() - dt.timedelta(hours=1)
-    interval_start = one_hour_ago.strftime("%Y-%m-%dT%H:00:00Z")
-    interval_end = one_hour_ago.strftime("%Y-%m-%dT%H:59:59Z")
-
-    process_endpoint_block(config, interval_start, interval_end, metrics_token)
+    config = Config()
+    endpoint = load_endpoint("/opt/config.yaml")
+    process_endpoint(endpoint, config)
