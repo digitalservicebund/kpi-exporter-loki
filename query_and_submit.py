bbd246baf48b171fc6fe37f67ad92cd0ac4fbd4f
Niko Felger <niko.felger@gmail.com>
2022-08-08 15:19:50 +0200

    Format code

--- query_and_submit.py
+++ query_and_submit.py
@@ -8,7 +8,7 @@ import yaml
 metrics_token = os.environ.get("METRICS_WEBHOOK_TOKEN")
 
 with open("/opt/config.yaml") as f:
-  config = yaml.safe_load(f)
+    config = yaml.safe_load(f)
 
 one_hour_ago = dt.datetime.now() - dt.timedelta(hours=1)
 interval_start = one_hour_ago.strftime("%Y-%m-%dT%H:00:00Z")
@@ -17,17 +17,27 @@ interval_end = one_hour_ago.strftime("%Y-%m-%dT%H:59:59Z")
 metrics = {}
 
 for metric_name, query in config["queries"].items():
-  output = json.loads(subprocess.check_output(["/logcli-linux-amd64", "instant-query", "--now", interval_end, f"sum(count_over_time({query} [1h]))"]))
-  metrics[metric_name] = output[0]["value"][1] if output else 0
+    output = json.loads(
+        subprocess.check_output(
+            [
+                "/logcli-linux-amd64",
+                "instant-query",
+                "--now",
+                interval_end,
+                f"sum(count_over_time({query} [1h]))",
+            ]
+        )
+    )
+    metrics[metric_name] = output[0]["value"][1] if output else 0
 
 requests.post(
-  url=config["endpoint"],
-  json={
-    "startInterval": interval_start,
-    "endInterval": interval_end,
-    **metrics,
-  },
-  headers={
-    "xc-token": metrics_token,
-  }
+    url=config["endpoint"],
+    json={
+        "startInterval": interval_start,
+        "endInterval": interval_end,
+        **metrics,
+    },
+    headers={
+        "xc-token": metrics_token,
+    },
 )
